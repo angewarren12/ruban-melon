c06872828812149ba0fbb8476adcf4b7efe44117
angewarren12 <angedesirecamara@gmail.com>
2025-12-09 21:57:32 +0000

    Fix: relative paths for Netlify

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // IMPORTANT: Force relative paths for assets
+  build: {
+    outDir: 'dist',
+  }
+  // Pas de "base" spécifié = par défaut c'est "/" (ce qu'on veut pour Netlify)
 })
